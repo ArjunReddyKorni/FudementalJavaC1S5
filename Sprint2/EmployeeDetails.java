39eeffd25169000973506e78cddd53577838060e
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-07-29 15:22:05 +0530

    practice1

---
+++ Sprint2/EmployeeDetails.java
@@ -0,0 +1,10 @@
+public class EmployeeDetails{
+	public static void main(String[] args){
+		String name = "Larry";
+		int age = 28;
+		double monthlySalary=2500.78;
+		System.out.println("name= "+name);
+		System.out.println("age= "+age);
+		System.out.println("montlySalary = $"+monthlySalary);
+	}
+}
